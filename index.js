adcd2a27a80ac725c1055571ef974ea9e929bef4
Raphael Waltenspül <raphael.waltenspuel@students.fhnw.ch>
2017-12-03 18:50:40 +0100

    test6

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(request, response) {
-    fs.readFile('index.html', function (err, html) {
         response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(html);
+        response.write('<img src="pic/index.jpg">');
         response.end();
-    });
 });
 
 var port = process.env.PORT || 1337;
