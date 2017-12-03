33c11378a37c6d6bd4c31fe74e8accdd1c2c7da3
Raphael Waltenspül <raphael.waltenspuel@students.fhnw.ch>
2017-12-03 17:51:18 +0100

    test2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
+    response.writeHead(200, { 'Content-Type': 'text/html'});
     response.write('<h1>Hallo</h1>');
     response.end("Hello World! KpiWebGui");
 
